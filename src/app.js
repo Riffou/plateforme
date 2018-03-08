53998eea7c1987f4703d74458ba98b8dba763b88
Nicolas Riffard <nicolas.riffard@lab-solutec.fr>
2018-03-08 14:48:07 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,39 +1,46 @@
-const express = require('express')
-const app = express()
-const myFunctions = require('./functions.js')
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var index = require('./routes/index');
+var users = require('./routes/users');
 
+var app = express();
 
-// Use it with "localhost:3000/calcul?premier=6&nbr1=5&nbr2=15" for example
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get('/calcul', function(req, res) {
-   // var premier = req.param('premier');
-  const nbr1 = parseInt(req.query.nbr1);
-  const nbr2 = parseInt(req.query.nbr2);  
-  const input_test_premier = parseInt(req.query.premier);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  const pgcd = myFunctions.pgcd(nbr1, nbr2);
+app.use('/', index);
+app.use('/users', users);
 
-  res.write(JSON.stringify(myFunctions.prem(input_test_premier)));
-	res.write(JSON.stringify('Le pgcd de '+ nbr1 +' et '+ nbr2 +' est '+ pgcd));
-	res.end()
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-var cache = {};
-
-app.post('/set', function(req, res) {
-  var query = req.query;
-  Object.keys(query).forEach(function(key) {
-    cache[key] = query[key];
-  });
-  res.status(200).end();
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error.ejs');
 });
 
-app.get('/get', function(req, res) {
-  res.send(cache[req.query.key]);
-});
-
-
-module.exports = app
+module.exports = app;
