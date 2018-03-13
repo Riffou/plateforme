c89d428f895bfca99307a1899b6680dd83e4ee26
Nicolas Riffard <nicolas.riffard@lab-solutec.fr>
2018-03-13 11:36:51 +0100

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,17 @@ router.get('/', function(req, res) {
     res.render('home.ejs');
 });
 
-router.get('/cours/', function(req, res) {
-    res.render('cours.ejs');
+router.get('/unites/', function(req, res) {
+    res.render('unites.ejs');
 });
 
-router.get('/cours/:numeroUnite', function(req, res) {
+router.get('/unites/:numeroUnite', function(req, res) {
     var unite = 'unite' + req.params.numeroUnite + '.ejs';
+    res.render(unite);
+});
+
+router.get('/unites/:numeroUnite/partie/:numeroPartie', function(req, res) {
+    var unite = 'unite' + req.params.numeroUnite + '_' + req.params.numeroPartie + '.ejs';
     res.render(unite);
 });
 
@@ -23,9 +28,11 @@ router.get('/faq/', function(req, res) {
     res.render('faq.ejs');
 });
 
+// Exemples du cours
+
 router.get('/exemples/:nomExemple', function(req, res) {
     var exemple = req.params.nomExemple + '.ejs';
-    res.render(exemple, {recherche: req.body.article});
+    res.render(exemple);
 });
 
 router.post('/exemples/:nomExemple', function(req, res) {
