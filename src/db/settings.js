f6fcd0f90374572450fc66e65f216da2cce4c96c
nriffard <nicolas.riffard@lab-solutec.fr>
2018-03-15 15:27:35 +0100

    DataBase

---
+++ src/db/settings.js
@@ -0,0 +1,12 @@
+// configuration de la connexion à la base de données
+var settings = {
+    host: 'localhost',
+    port: 5432,
+    database: 'ensimag',
+    user: 'ensimag',
+    password: 'ensimagdb'
+};
+
+module.exports = {
+    settings
+}
